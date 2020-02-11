b7348461592252f98123db81b77f2fa054981179
Joel Helbling <jh2015@kent.ac.uk>
2020-02-11 19:11:33 +0000

    Fixed a typo in MC enum

--- Position.java
+++ Position.java
@@ -1,4 +1,20 @@
 public class Position {
+    public enum Department{
+        Admin("Administration"),
+        SM("Sales and Marketing"),
+        IT("Information Technology"),
+        BI("Business Intelligence"),
+        MC("Management Consultancy"),
+        HR("Human Resources");
+
+        public final String label;
+
+        private Department(String label)
+        {
+            this.label = label;
+        }
+    }
+
     public enum Role {
         Employee("Employee"),
         Manager("Manager"),
@@ -7,22 +23,6 @@ public class Position {
         public final String label;
 
         private Role(String label)
-        {
-            this.label = label;
-        }
-    }
-
-    public enum Department{
-        Admin("Administration"),
-        SM("Sales and Marketing"),
-        IT("Information Technology"),
-        BI("Business Intelligence"),
-        MC("Management Consultency"),
-        HR("Human Resources");
-
-        public final String label;
-
-        private Department(String label)
         {
             this.label = label;
         }
