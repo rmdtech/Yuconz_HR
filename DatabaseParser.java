a5c6f5bea4fb6116d27fbd64738fc5ab402858db
Spatchy <spatchy@spatchy.net>
2020-02-12 00:53:36 +0000

    Added stubs

--- DatabaseParser.java
+++ DatabaseParser.java
@@ -1,2 +1,50 @@
-public class DatabaseParser {
+import java.sql.*;
+
+
+public class DatabaseParser
+{
+    Connection c = null;
+    public DatabaseParser()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:databases/yuconz.db");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    Boolean recordAuthorisationAttempt(String employeeId, Object deleteMe, Object deleteMe2, Object deleteMe3, Boolean Successful)
+    {
+        return true;
+    }
+
+    Boolean checkEmployeeID(String employeeId)
+    {
+        return true;
+    }
+
+    Boolean getEmployeePassword(String employeeId)
+    {
+        return true;
+    }
+
+    Boolean getPasswordSalt(String employeeId)
+    {
+        return true;
+    }
+
+    String fetchDepartment(String employeeId)
+    {
+        return "ChangeMe";
+    }
+
+    String fetchRole(String employeeId)
+    {
+        return "ChangeMe";
+    }
 }
