8fe2913c2b67228be9d01f057b91c540c4b0a00f
Joel Helbling <jh2015@kent.ac.uk>
2020-02-28 14:59:12 +0000

    Added sample users in setup and teardown

--- AuthoriseTest.java
+++ AuthoriseTest.java
@@ -1,16 +1,37 @@
+import java.util.zip.DataFormatException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthoriseTest {
-
+    User hrEmployee;
+    User itEmployee;
+    User hrManager;
     @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    void setUp()
+    {
+        Authenticate.addNewUser("hre123", "password", Position.Department.HR, Position.Role.Employee);
+        Authenticate.addNewUser("ite123", "password", Position.Department.IT, Position.Role.Employee);
+        Authenticate.addNewUser("hrm123", "password", Position.Department.IT, Position.Role.Employee);
+        hrEmployee = Authenticate.login("hre123", "password");
+        itEmployee = Authenticate.login("ite123", "password");
+        hrManager = Authenticate.login("hrm123", "password");
     }
 
     @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    void tearDown()
+    {
+        DatabaseParser dp = new DatabaseParser();
+        dp.deleteSession(hrEmployee.getSessionId());
+        dp.sqlUpdate("DELETE * FROM User WHERE employeeId = hre123");
+        dp.deleteSession(itEmployee.getSessionId());
+        dp.sqlUpdate("DELETE * FROM User WHERE employeeId = ite123");
+        dp.deleteSession(hrManager.getSessionId());
+        dp.sqlUpdate("DELETE * FROM User WHERE employeeId = hrm123");
     }
 
     @org.junit.jupiter.api.Test
-    void authorisationAttempt() {
+    void authorisationAttempt()
+    {
+
     }
 }
