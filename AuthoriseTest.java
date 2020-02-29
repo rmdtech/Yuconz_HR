b5bbbdb8f939c58840956460aed79999b513f615
ntjs2 <ntjs2@kent.ac.uk>
2020-02-29 23:32:46 +0000

    Added Update and Delete tests for Authorise

--- AuthoriseTest.java
+++ AuthoriseTest.java
@@ -42,8 +42,6 @@ class AuthoriseTest {
     @org.junit.jupiter.api.Test
     void createPersonalDetails()
     {
-        setUp();
-        System.out.println("---- Test output here --- ");
         String[] hre123FullPayload = { "hre123", "Roman", "Miles", "01/01/1970", "University of Kent", "CT2 7NF", "Canterbury", "Kent", "01227748392", "07638270376", "David Barnes", "01227827696"};
         String[] emptyPayload = { null, null, null, null, null, null, null, null, null, null, null };
         String[] ite123FullPayload = { "ite123", "Smith", "John", "01/01/1970", "University of Kent", "Canterbury", "CT2 7NF", "Kent", "01227748392", "07638270376", "David Barnes", "01227827696"};
@@ -68,11 +66,16 @@ class AuthoriseTest {
     }
 
     @org.junit.jupiter.api.Test
-    void updatePersonalDetails()
+    void  updatePersonalDetails()
     {
+        String[] hre123FullPayload = { "hre123", "Roman", "Miles", "01/01/1970", "University of Kent", "CT2 7NF", "Canterbury", "Kent", "01227748392", "07638270376", "David Barnes", "01227827696"};
+        String[] ite123FullPayload = { "ite123", "Smith", "John", "01/01/1970", "University of Kent", "Canterbury", "CT2 7NF", "Kent", "01227748392", "07638270376", "David Barnes", "01227827696"};
         String[] hreFullPayload = {"hre123", "Roman", "Miles", "01/01/1970", "University of Kent", "Canterbury", "Kent", "CT2 7NF", "01227748392", "07638270376", "Olaf Chitil", "01227824320"};
         String[] iteFullPayload = {"ite123", "Smith", "John", "01/01/1970", "University of Kent", "Canterbury", "Kent", "CT2 7NF", "01227748392", "07638270376", "Olaf Chitil", "01227824320"};
         String[] emptyPayload = {null, null, null, null, null, null, null, null, null, null, null};
+
+        Authorise.AuthorisationAttempt(Authorise.Action.Create, "Personal Details", hrEmployee, hre123FullPayload);
+        Authorise.AuthorisationAttempt(Authorise.Action.Create, "Personal Details", hrManager, ite123FullPayload);
 
         // Variant 1: Expected Use, user updates their own info
         assertTrue(Authorise.AuthorisationAttempt(Authorise.Action.Update, "Personal Details", itEmployee, iteFullPayload));
@@ -85,6 +88,7 @@ class AuthoriseTest {
 
         // Variant 4: Unexpected Use, Try to update with empty payload
         assertTrue(Authorise.AuthorisationAttempt(Authorise.Action.Update, "Personal Details", hrEmployee, emptyPayload));
+
 
         // Variant 5: Unexpected Use, User is neither HR or updating their own info
         assertFalse(Authorise.AuthorisationAttempt(Authorise.Action.Update, "Personal Details", itEmployee, hreFullPayload));
