e44de3beb0b75fa6e5d92763fa245059321fe912
jh2015 <helblingjoel@gmail.com>
2020-03-01 22:06:10 +0000

    Translated previous testing method to JUnit tests

--- AuthenticateTest.java
+++ AuthenticateTest.java
@@ -1,204 +1,97 @@
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class AuthenticateTest {
-    public static boolean testEncryption()
-    {
-        System.out.println("\n----- Encryption -----");
-        System.out.println("   Test: Encrypt known value");
-        String testPassword = "password";
-        String testSalt = "salt";
-        String expected = "fa6a2185b3e0a9a85ef41ffb67ef3c1fb6f74980f8ebf970e4e72e353ed9537d593083c201dfd6e43e1c8a7aac2bc8dbb119c7dfb7d4b8f131111395bd70e97f";
-        String generated = Authenticate.sha512Encrypt(testPassword, testSalt);
-        if (generated.equals(expected))
-        {
-            System.out.println("      [✓]    Expected String matches generated String");
-            System.out.println("All encyrption tests have passed");
-            return true;
-        }
-        System.out.println("      [x]    Strings don't match");
-        System.out.println("         Expected: " + expected);
-        System.out.println("        Generated: " + generated);
-        return false;
+    DatabaseParser dp = new DatabaseParser();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        dp.sqlUpdate("DELETE FROM Session");
+        dp.sqlUpdate("DELETE FROM AuthorisationLog");
+        dp.sqlUpdate("DELETE FROM AuthenticationLog");
+        dp.sqlUpdate("DELETE FROM User");
     }
 
-    public static Integer testLogin()
-    {
-        int failures = 0;
-        System.out.println("\n----- Login tests -----");
-        System.out.println("   Test: Correct Login 'abc123' 'password'");
-        User abc123 = Authenticate.login("abc123", "password");
-        if (abc123 != null)
-        {
-            if (abc123.getEmployeeId().equals("abc123"))
-            {
-                if (abc123.getSessionId() != null)
-                {
-                    System.out.println("   [Status] SessionID: " + abc123.getSessionId());
-                    System.out.println("     [✓]    User was logged in");
-                }
-                else
-                {
-                    System.out.println("     [x]    User object created, but no sessionID written");
-                    failures++;
-                }
-            }
-            else
-            {
-                System.out.println("     [x]    Logged in user is: " + abc123.getEmployeeId());
-                failures++;
-            }
-        }
-        else
-        {
-            System.out.println("     [x]    Test failed. User Element not returned");
-            failures++;
-        }
-        System.out.println("   Correct Login tests concluded");
 
-        System.out.println("\n   Test: Incorrect password");
-        User wrongPassword = Authenticate.login("abc123", "wrongpassword");
-        if (wrongPassword == null)
-        {
-            System.out.println("     [✓]    User not logged in");
-        }
-        else
-        {
-            System.out.println("     [x]    Test failed. User has been logged in");
-            System.out.println(wrongPassword.getEmployeeId());
-            failures++;
-        }
-        System.out.println("   Incorrect password tests concluded\n");
+    String sqlRead(String query, String coloumn) {
+        String row = "";
+        dp.sqlRead(query);
+        try {
+            dp.result.next(); // only ever be one result, while loop not required
+            row = dp.result.getString(coloumn);
+            dp.result.close();
+            dp.stmt.close();
+        } catch (SQLException e) {
 
-        System.out.println("   Test: Incorrect employeeId");
-        User wrongEmpID = Authenticate.login("err404", "password");
-        if (wrongEmpID == null)
-        {
-            System.out.println("     [✓]    User not logged in");
         }
-        else
-        {
-            System.out.println("     [x]    Test failed. User has been logged in");
-            System.out.println(wrongEmpID.getEmployeeId());
-            failures++;
-        }
-        System.out.println("   Incorrect employeeID tests concluded");
-        return failures;
+        return row;
     }
 
-    public static Integer testLogout()
-    {
-        int failures = 0;
-        System.out.println("\n----- Logout tests -----");
-        System.out.println("   [Status] Loggin in user 'abc123'");
-        User abc123 = Authenticate.login("abc123", "password");
-        System.out.println("   [Status] sessionID: " + abc123.getSessionId());
-        Authenticate.logout(abc123);
-        System.out.print("MANUALLY CHECK IF SESSIONID IS STILL IN THE DATABASE");
-        System.out.println("Logout test requires manual verification");
-        return failures;
+    @org.junit.jupiter.api.Test
+    void sha512encrypt() {
+        assertEquals("fa6a2185b3e0a9a85ef41ffb67ef3c1fb6f74980f8ebf970e4e72e353ed9537d593083c201dfd6e43e1c8a7aac2bc8dbb119c7dfb7d4b8f131111395bd70e97f", Authenticate.sha512Encrypt("password", "salt"));
     }
 
-    public static Integer testAddNewUser()
+    @org.junit.jupiter.api.Test
+    void testAddNewValidUser()
     {
-        int fails = 0;
-        String password = "password";
-
-        System.out.println("\n----- addNewUser tests -----");
-        System.out.println("   Test: Add valid employeeID");
-        String validEmpID = newEmployeeID();
-        if (Authenticate.addNewUser(validEmpID, password, Position.Department.IT, Position.Role.Employee))
-        {
-            System.out.println("     [✓]    employeeId Written: " + validEmpID);
-        }
-        else
-        {
-            System.out.println("     [x]    " + validEmpID + " has not been added");
-            fails++;
-        }
-        System.out.println("   valid employeeID test concluded");
-
-        System.out.println("\n   Test: Add invalid (long) employeeID");
-        if(Authenticate.addNewUser("invalidName", password, Position.Department.IT, Position.Role.Employee))
-        {
-            System.out.println("     [x]    invalidName has been added when it should have not");
-            fails++;
-        }
-        else
-        {
-            System.out.println("     [✓]    invalidName has not been added");
-        }
-        System.out.println("   add invalid (long) employeeID test concluded");
-
-        System.out.println("\n   Test: Add already existing employeeID");
-        if(Authenticate.addNewUser("abc123", password, Position.Department.IT, Position.Role.Employee))
-        {
-            System.out.println("     [x]    already existing employee has been overwritten");
-            fails++;
-        }
-        else
-        {
-            System.out.println("     [✓]    already existing entry has been rejected");
-        }
-        System.out.println("   add already existing employeeID test concluded");
-
-        System.out.println("\n   Test: Add invalid, but same sized employeeID");
-        if(Authenticate.addNewUser("testin", password, Position.Department.IT, Position.Role.Employee))
-        {
-            System.out.println("     [x]    testin has been added when it should have not");
-            fails++;
-        }
-        else
-        {
-            System.out.println("     [✓]    testin has not been added");
-        }
-        System.out.println("   add invalid, but same sized employeeID test concluded");
-
-        /* After fixing Issue #4 this test is no longer applicable
-        System.out.println("\n   Test: Add User with invalid Department");
-        String invalidDptEmpId = newEmployeeID();
-        if(Authenticate.addNewUser(invalidDptEmpId, password, "Invalid Department", "Employee"))
-        {
-            System.out.println("     [x]    department was invalid but user has been added regardless");
-            fails++;
-        }
-        else
-        {
-            System.out.println("     [✓]    department was invalid and user has not been added");
-        }
-        System.out.println("   add User with invalid Department test concluded");
-        */
-
-        /* After fixing issue #4 this test is no longer applicable
-        System.out.println("\n   Test: Add User with invalid Role");
-        String invalidRoleEmpID = newEmployeeID();
-        if(Authenticate.addNewUser(invalidRoleEmpID, password, "IT", "Invalid Role"))
-        {
-            System.out.println("     [x]    role was invalid but user has been added regardless");
-            fails++;
-        }
-        else
-        {
-            System.out.println("     [✓]    role was invalid and user has not been added");
-        }
-        System.out.println("   add invalid (long) employeeID test concluded");
-        */
-
-        return fails;
+        assert(Authenticate.addNewUser("abc123", "password", Position.Department.IT, Position.Role.Employee));
     }
 
-    private static Integer randomNumericID()
+    @org.junit.jupiter.api.Test
+    void testAddNewLongEmployeeId()
     {
-        double x = (Math.random()*((999-100)+1))+100;
-        return (int)x;
+        assertFalse(Authenticate.addNewUser("invalidName", "password", Position.Department.IT, Position.Role.Employee));
     }
 
-    public static String newEmployeeID()
+    @org.junit.jupiter.api.Test
+    void testAddNewShortEmployeeId()
     {
-        // For valid employeeId, make sure this test ID hasn't already been used
-        DatabaseParser dp = new DatabaseParser();
-        String employeeId = "tes" + randomNumericID();
-        while(dp.checkEmployeeId(employeeId))
-        {
-            employeeId = "tes" + randomNumericID();
-        }
-        return employeeId;
+        assertFalse(Authenticate.addNewUser("ab12", "password", Position.Department.IT, Position.Role.Employee));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddNewInvalidEmployeeId()
+    {
+        assertFalse(Authenticate.addNewUser("abcdef", "password", Position.Department.IT, Position.Role.Employee));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddExistingUser()
+    {
+        Authenticate.addNewUser("abc123", "password", Position.Department.IT, Position.Role.Employee);
+        assertFalse(Authenticate.addNewUser("abc123", "password", Position.Department.IT, Position.Role.Employee));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testValidLogin() {
+        Authenticate.addNewUser("abc123", "password", Position.Department.HR, Position.Role.Employee);
+        assertNotNull(Authenticate.login("abc123", "password"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testInvalidPassword() {
+        Authenticate.addNewUser("abc123", "password", Position.Department.HR, Position.Role.Employee);
+        assertNull(Authenticate.login("abc123", "wrongpassword"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testInvalidUserId() {
+        assertNull(Authenticate.login("err404", "password"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLogout() {
+        Authenticate.addNewUser("abc123", "password", Position.Department.HR, Position.Role.Employee);
+        User testUser = Authenticate.login("abc123", "password");
+
+        String beforeLogout = sqlRead("SELECT * FROM Session", "employeeId");
+
+        Authenticate.logout(testUser);
+
+        String afterLogout = sqlRead("SELECT * FROM Session", "employeeId");
+
+        assertNotEquals(beforeLogout, afterLogout);
     }
 }
--- AuthoriseTest.java
+++ AuthoriseTest.java
@@ -1,98 +1,51 @@
-public class AuthoriseTest
-{
-    public static Integer testAuthorisation()
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthoriseTest {
+
+    DatabaseParser dp = new DatabaseParser();
+
+    @BeforeEach
+    void setUp()
     {
-        int fails = 0;
-        System.out.println("\n----- Authorisation -----");
-        System.out.println("   Test: Attempt action when not logged in (no sessionID)");
-        User nullSessionIdUser = new User(AuthenticateTest.newEmployeeID(), null);
-        nullSessionIdUser.setDepartment(Position.Department.HR);
-        nullSessionIdUser.setRole(Position.Role.Employee);
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, nullSessionIdUser))
-        {
-            System.out.println("      [x]    " + nullSessionIdUser.getEmployeeId() + " was not logged in");
-            fails++;
-        }
-        else
-        {
-            System.out.println("      [✓]    Action has been rejected, as expected");
-        }
-        System.out.println("   Attempt action when not logged in (no sessionID) concluded");
+        dp.sqlUpdate("DELETE FROM Session");
+        dp.sqlUpdate("DELETE FROM AuthorisationLog");
+        dp.sqlUpdate("DELETE FROM AuthenticationLog");
+        dp.sqlUpdate("DELETE FROM User");
+    }
 
-        System.out.println("\n   Test: Attempt action when not logged in (invalid sessionID)");
-        User invalidSessionIdUser = new User(AuthenticateTest.newEmployeeID(), "invalidSessionID");
-        invalidSessionIdUser.setDepartment(Position.Department.HR);
-        invalidSessionIdUser.setRole(Position.Role.Employee);
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, invalidSessionIdUser))
-        {
-            System.out.println("      [x]    " + invalidSessionIdUser.getEmployeeId() + " was not logged in");
-            fails++;
-        }
-        else
-        {
-            System.out.println("      [✓]    Action has been rejected, as expected");
-        }
-        System.out.println("   Attempt action when not logged in (invalid sessionID) concluded");
+    @Test
+    void authorisationAttemptNoLogin()
+    {
+        Authenticate.addNewUser("abc123", "password", Position.Department.IT, Position.Role.Employee);
+        User testUser = Authenticate.login("abc123", "password");
+        Authenticate.logout(testUser);
+        assertFalse(Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, testUser));
+    }
 
-        System.out.println("\n   Test: Attempt action from wrong department");
-        String wrongDepartmentID = AuthenticateTest.newEmployeeID();
+    @Test
+    void authorisationAttemptWrongDepartment()
+    {
+        Authenticate.addNewUser("abc123", "password", Position.Department.BI, Position.Role.Employee);
+        User testUser = Authenticate.login("abc456", "password");
+        assertFalse(Authorise.AuthorisationAttempt("Read", "abc456 - Personal Details", Position.Department.HR, Position.Role.Employee, testUser));
+    }
 
-        Authenticate.addNewUser(wrongDepartmentID, "password", Position.Department.BI, Position.Role.Employee);
+    @Test
+    void authorisationAttemptWrongRole()
+    {
+        Authenticate.addNewUser("abc123", "password", Position.Department.HR, Position.Role.Employee);
+        User testUser = Authenticate.login("abc123", "password");
+        assertFalse(Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Manager, testUser));
+    }
 
-        User  wrongDepartmentUser = Authenticate.login(wrongDepartmentID, "password");
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, wrongDepartmentUser))
-        {
-            System.out.println("      [x]    " + wrongDepartmentUser.getEmployeeId() + " was in the wrong department but action got authorised");
-            fails++;
-        }
-        else
-        {
-            System.out.println("      [✓]    Action has been rejected, as expected");
-        }
-        System.out.println("   Attempt action from wrong department concluded");
-
-        System.out.println("\n   Test: Attempt action from wrong role");
-        String wrongRoleID = AuthenticateTest.newEmployeeID();
-        Authenticate.addNewUser(wrongRoleID, "password", Position.Department.BI, Position.Role.Employee);
-        User  wrongRoleUser = Authenticate.login(wrongDepartmentID, "password");
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, wrongRoleUser))
-        {
-            System.out.println("      [x]    " + wrongRoleUser.getEmployeeId() + " did not meet the min. role but still got authorised");
-            fails++;
-        }
-        else
-        {
-            System.out.println("      [✓]    Action has been rejected, as expected");
-        }
-        System.out.println("   Attempt action from wrong role concluded");
-
-        System.out.println("\n   Test: Attempt action with uninitiated User");
-        String incompleteUserId = AuthenticateTest.newEmployeeID();
-        User  incompleteUser = new User(incompleteUserId, "invalidSession");
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, incompleteUser))
-        {
-            System.out.println("      [x]    " + incompleteUser.getEmployeeId() + " user is not initialised properly yet action to accepted");
-            fails++;
-        }
-        else
-        {
-            System.out.println("      [✓]    Action has been rejected, as expected");
-        }
-        System.out.println("   Attempt action with uninitiated User concluded");
-
-        System.out.println("\n   Test: Attempt action with valid User");
-        User trueUser = Authenticate.login("abc123", "password");
-        if (Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, trueUser))
-        {
-            System.out.println("      [✓]    Action has been authorised as intended");
-        }
-        else
-        {
-            System.out.println("      [x]    Action got denied despite valid credentials");
-            fails++;
-        }
-        System.out.println("   Attempt action with valid User concluded");
-        System.out.println("All Authorise tests have finished");
-        return fails;
+    @Test
+    void authorisationAttemptTrue()
+    {
+        Authenticate.addNewUser("abc123", "password", Position.Department.HR, Position.Role.Employee);
+        User testUser = Authenticate.login("abc123", "password");
+        assertTrue(Authorise.AuthorisationAttempt("Read", "abc123 - Personal Details", Position.Department.HR, Position.Role.Employee, testUser));
     }
 }
