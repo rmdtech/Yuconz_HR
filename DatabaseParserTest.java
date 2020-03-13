2bd28e884e591c8eb1aa39c87cb85ee32527851e
Spatchy <spatchy@spatchy.net>
2020-03-13 17:55:44 +0000

    Created filterNulls method to fix issue #19

--- DatabaseParserTest.java
+++ DatabaseParserTest.java
@@ -1,12 +1,38 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseParserTest {
 
+    DatabaseParser dp = new DatabaseParser();
+
+    static void initDatabase() {
+        String[] arguments = new String[] {"123"};
+        Main.main(arguments);
+    }
+
+    static void deleteDatabase() {
+        File dbFile = new File("./databases/yuconz.db");
+        dbFile.delete();
+    }
+
+    static void generateTestUsers() {
+        Authenticate.addNewUser("dir123", "password", null, Position.Department.HR, Position.Role.Director);
+        Authenticate.addNewUser("hrm123", "password", "dir123", Position.Department.HR, Position.Role.Manager);
+        Authenticate.addNewUser("hre123", "password", "hrm123", Position.Department.HR, Position.Role.Employee);
+        Authenticate.addNewUser("itm123", "password", "dir123", Position.Department.IT, Position.Role.Manager);
+        Authenticate.addNewUser("ite123", "password", "itm123", Position.Department.IT, Position.Role.Employee);
+    }
+
     @Test
     void createReview() {
-
+        //initDatabase();
+        generateTestUsers();
+        String[] payload = {"hre123", "2020-03-23", "hrm123", "itm123", "69d76bd5a1ae48a284587698cf980fa6"};
+        assertTrue(dp.createReview(payload));
+        deleteDatabase();
     }
 
     @Test
