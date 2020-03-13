a955f93b2b41b50fac6701527bf9c709dcb0e039
Spatchy <spatchy@spatchy.net>
2020-03-13 15:38:17 +0000

    Created DatabaseParserTest class

---
+++ DatabaseParserTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseParserTest {
+
+    @Test
+    void createReview() {
+    }
+
+    @Test
+    void fetchReviewDocumentId() {
+    }
+
+    @Test
+    void isReviewer() {
+    }
+
+    @Test
+    void isReviewee() {
+    }
+
+    @Test
+    void fetchReview() {
+    }
+
+    @Test
+    void fetchPastPerformance() {
+    }
+
+    @Test
+    void fetchFuturePerformance() {
+    }
+
+    @Test
+    void updateReview() {
+    }
+}
