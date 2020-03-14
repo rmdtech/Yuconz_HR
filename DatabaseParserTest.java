9f73971b47cfb33424ff56d8a293e0047391820b
Spatchy <spatchy@spatchy.net>
2020-03-14 17:34:24 +0000

    Removed needless imports

--- DatabaseParserTest.java
+++ DatabaseParserTest.java
@@ -1,12 +1,5 @@
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
