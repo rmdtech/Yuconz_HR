62413b2c4ad69ab700502c99a9905070edad0458
robert <rmdishman@outlook.com>
2020-03-16 22:45:56 +0000

    Added working scene switching method and 2 working scenes.

--- YuconzGui.java
+++ YuconzGui.java
@@ -1,42 +1,86 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 
 public class YuconzGui extends Application {
 
-    String absPath;
+    public TextField employeeIdField;
+    public PasswordField passwordField;
+    User user = null;
+    FXMLLoader loader = new FXMLLoader();
 
     public YuconzGui() {
-        absPath = new File("Login.fxml").getAbsolutePath();
+
     }
 
     public static void main(String[] args) {
         Application.launch(args);
     }
 
+    public String getAbsPath(String filename)
+    {
+        String path =  "file:///" + new File(filename).getAbsolutePath();
+        System.out.println(path);
+        return path;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Yuconz");
-
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(new URL("file:///" + absPath));
-
+        loader.setLocation(new URL(getAbsPath("Login.fxml")));
         AnchorPane anchorPane = loader.<AnchorPane>load();
-        Scene scene = new Scene(anchorPane);
-        primaryStage.setScene(scene);
-
+        Scene loginScene = new Scene(anchorPane);
+        primaryStage.setScene(loginScene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
-    public void doThing(ActionEvent actionEvent) {
-        System.out.println("Thing Done!");
+    public void changeScene(String fxml) throws Exception
+    {
+        Stage stage = (Stage) Stage.getWindows().stream().filter(Window::isShowing).findFirst().orElse(null);
+        loader.setLocation(new URL(getAbsPath(fxml)));
+        AnchorPane anchorPane = loader.<AnchorPane>load();
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+
+    public void login(ActionEvent actionEvent) throws Exception {
+
+        String employeeId = employeeIdField.getText();
+        String password = passwordField.getText();
+        user = Authenticate.login(employeeId, password);
+
+        if(user != null)
+        {
+
+        }
+        else
+        {
+            changeScene("ProfilePage.fxml");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Login Form Error!");
+            alert.setContentText("Your username or password is incorrect!");
+            alert.showAndWait();
+        }
+
+
     }
 }
