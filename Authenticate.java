af7b2b8d61b15d6440e98b330bd2b1a01b056ac2
jh2015 <helblingjoel@gmail.com>
2020-03-17 14:16:47 +0000

    Fixed an issue raised in in the Meeting 17/03/2020 v2

--- Authenticate.java
+++ Authenticate.java
@@ -68,10 +68,14 @@ public class Authenticate{
             System.out.println("No supervisor with this ID exists");
             return false;
         }
-        if (dp.fetchRole(supervisor) != null && (dp.fetchRole(supervisor).level <= (Position.Role.Employee.level)))
+
+        if (dp.fetchRole(supervisor) != null)
         {
-            System.out.println("Supervisor cannot be of the same or lower level than the User you are trying to add");
-            return false;
+            if ((dp.fetchRole(supervisor).level <= (Position.Role.Employee.level))
+            {
+                System.out.println("Supervisor cannot be of the same or lower level than the User you are trying to add");
+                return false;
+            }
         }
 
         dp.newEmployee(employeeId, passwordSalt, sha512Encrypt(password, passwordSalt), supervisor ,department.label, role.label);
