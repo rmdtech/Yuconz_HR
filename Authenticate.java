b60a143758fb893f222ca694eaae9b908d0e6a64
Joel Helbling <jh2015@kent.ac.uk>
2020-03-18 15:48:50 +0000

    Merged over changes from Testing branch where merge was not successful

--- Authenticate.java
+++ Authenticate.java
@@ -3,7 +3,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.ArrayList;
 
 public class Authenticate{
     private static DatabaseParser dp = new DatabaseParser();
@@ -48,9 +47,9 @@ public class Authenticate{
      * @param role Their role within the company
      * @return If the action has been successful
      */
-    public static boolean addNewUser(String employeeId, String password, Position.Department department, Position.Role role)
+    public static boolean addNewUser(String employeeId, String password, String supervisor, Position.Department department, Position.Role role)
     {
-        String passwordSalt = User.generateSalt();
+        String passwordSalt = User.generateUUID();
 
         Matcher employeeIdMatcher = Pattern.compile("[a-z]{3}[0-9]{3}").matcher(employeeId);
         if (!employeeIdMatcher.matches())
@@ -63,20 +62,30 @@ public class Authenticate{
             System.out.println("An employee with ID: "+ employeeId + " already exists");
             return false;
         }
+        if (supervisor != null && !dp.checkEmployeeId(supervisor))
+        {
+            System.out.println("No supervisor with this ID exists");
+            return false;
+        }
+        if (dp.fetchRole(supervisor) != null && dp.fetchRole(supervisor).level <= (Position.Role.Employee.level))
+        {
+            System.out.println("Supervisor cannot be of the same or lower level than the User you are trying to add");
+            return false;
+        }
 
-        dp.newEmployee(employeeId, passwordSalt, sha512Encrypt(password, passwordSalt), department.label, role.label);
+        dp.newEmployee(employeeId, passwordSalt, sha512Encrypt(password, passwordSalt), supervisor ,department.label, role.label);
         System.out.println("New User has been created and added to the Database");
         return true;
     }
 
     /**
      * Returns a given User object based on the employeeId given
-     * @param employeeId The employeeId of the User that should be found
+     * @param user The user object that should be checked
      * @return The User with such employee ID. If not found, this is null
      */
-    public static boolean isUserLoggedIn(String employeeId)
+    public static boolean isUserLoggedIn(User user)
     {
-        return dp.checkEmployeeId(employeeId);
+        return dp.isLoggedIn(user.getEmployeeId(), user.getSessionId());
     }
 
     /**
