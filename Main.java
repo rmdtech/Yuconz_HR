1eb68210101c0a7adf0f05c2a63f16f93d79112d
Spatchy <spatchy@spatchy.net>
2020-04-02 18:01:34 +0100

    Reformatted main method to run main method in YuconzGui

--- Main.java
+++ Main.java
@@ -1,19 +1,6 @@
-import java.io.File;
-
 public class Main {
-    public static void main(String[] args) {
-        if(checkIsFirstBoot())
-        {
-            File dir = new File("./databases");
-            dir.mkdir();
-            DatabaseParser dp = new DatabaseParser();
-            dp.setupDatabase();
-        }
-    }
-
-    static boolean checkIsFirstBoot()
+    public static void main(String[] args)
     {
-        File dbFile = new File("./databases/yuconz.db");
-        return !dbFile.exists();
+        YuconzGui.main(args);
     }
 }
